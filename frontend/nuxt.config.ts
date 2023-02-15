ed3965edbad6c0c0a21459407aff02c55d943f4a
dsdsdsdsdsds <dsdsdsdsdsds@users.noreply.github.com>
2023-02-15 17:40:22 +0100

    Prepare for favicon

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -13,6 +13,28 @@ export default defineNuxtConfig({
 
   app: {
     pageTransition: { name: 'page', mode: 'out-in' },
+
+    // head: {
+    //   // Place icon files in /public directory
+    //   link: [
+    //     {
+    //       // Size: 32x32px
+    //       rel: 'icon',
+    //       href: '/favicon.ico',
+    //       sizes: 'any',
+    //     },
+    //     {
+    //       rel: 'icon',
+    //       href: '/favicon.svg',
+    //       type: 'image/svg+xml',
+    //     },
+    //     {
+    //       // Size: 180x180px
+    //       rel: 'apple-touch-icon',
+    //       href: '/apple-touch-icon.png',
+    //     },
+    //   ],
+    // },
   },
 
   modules: ['@pinia/nuxt'],
