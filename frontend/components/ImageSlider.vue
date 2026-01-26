1a818a94e862365153b77224897c1f8d0ba821bb
npnpnpnpnpnp <19845311+npnpnpnpnpnp@users.noreply.github.com>
2026-01-26 16:31:59 +0100

    Fetch all data for project view

---
+++ frontend/components/ImageSlider.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+const props = defineProps<{
+  slides: Image[]
+}>()
+</script>
+
+<template>
+  <ul class="image-slider">
+    <ImageSlide
+      v-for="(slide, index) in props.slides"
+      :key="`slide-${index}`"
+      :slide="slide"
+    />
+  </ul>
+</template>
+
+<style lang="scss" scoped>
+// .image-slider {}
+</style>
