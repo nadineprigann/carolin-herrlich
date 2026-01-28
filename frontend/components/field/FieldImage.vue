c5e36274ac082988c3aad5271f26121c395a252f
npnpnpnpnpnp <19845311+npnpnpnpnpnp@users.noreply.github.com>
2026-01-28 13:19:22 +0100

    Fetch all image metadata and display it ARIA-safe

--- frontend/components/field/FieldImage.vue
+++ frontend/components/field/FieldImage.vue
@@ -1,4 +1,5 @@
 <script lang="ts" setup>
+import { useId } from 'vue'
 interface Props {
   image: Image
   sizes?: string
@@ -27,28 +28,74 @@ const srcset = computed(() => {
     })
     .join(',')
 })
+
+const text = computed(() => {
+  return {
+    license: 'Lizenz:  ',
+  }
+})
+
+// create unique ssr-safe id for aria-describedby. comes from vue 3.3 core.
+const id = useId()
+
+// show long description id only if long description exists to make it ARIA valid
+const longDescId = computed(() => {
+  return props.image?.long_description?.length ? `long-description-${id}` : null
+})
 </script>
 
 <template>
-  <img
-    v-if="props.image && props.image.resized"
-    :srcset="srcset"
-    :sizes="sizes"
-    :loading="props.loading"
-    :alt="props.image.alt_text"
-    :class="classes"
-    class="field-image"
-  />
-  <img
-    v-else-if="props.image && !props.image.resized"
-    :src="props.image.urls[0].url"
-    :loading="props.loading"
-    :alt="props.image.alt_text"
-    :class="classes"
-    class="field-image"
-  />
+  <figure v-if="props.image" class="field-image">
+    <img
+      v-if="props.image && props.image.resized"
+      :srcset="srcset"
+      :sizes="sizes"
+      :loading="props.loading"
+      :alt="props.image.alt_text"
+      :aria-describedby="longDescId"
+      :class="classes"
+      class="image"
+    />
+    <img
+      v-else-if="props.image && !props.image.resized"
+      :src="props.image.urls[0].url"
+      :loading="props.loading"
+      :alt="props.image.alt_text"
+      :aria-describedby="longDescId"
+      :class="classes"
+      class="image"
+    />
+    <figcaption class="image-caption">
+      <span class="caption">{{ props.image.caption }}</span>
+      <a class="link" :href="props.image.external_link">
+        <span> © {{ props.image.external_link_title }}</span>
+      </a>
+      <span v-if="props.image.year" class="year">, {{ props.image.year }}</span>
+      <span class="license"
+        >.&nbsp;{{ text.license }}{{ props.image.license.fields.title }}.
+      </span>
+    </figcaption>
+    <p
+      v-if="props.image.long_description.length"
+      :id="longDescId"
+      class="long-description"
+    >
+      {{ props.image.long_description }}
+    </p>
+  </figure>
 </template>
 
 <style lang="scss" scoped>
 // .field-image {}
+.long-description {
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  padding: 0;
+  margin: -1px;
+  overflow: hidden;
+  clip: rect(0 0 0 0);
+  white-space: nowrap;
+  border: 0;
+}
 </style>
