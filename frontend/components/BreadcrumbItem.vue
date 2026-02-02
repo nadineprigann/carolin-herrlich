f9375b7bef1ce5043da03415789034ba3b086f33
npnpnpnpnpnp <19845311+npnpnpnpnpnp@users.noreply.github.com>
2026-02-02 14:39:38 +0100

    Make item navigatable if its not the current page

--- frontend/components/BreadcrumbItem.vue
+++ frontend/components/BreadcrumbItem.vue
@@ -2,11 +2,27 @@
 const props = defineProps<{
   breadcrumb: Route
 }>()
+
+const isClickable = computed(() => {
+  return props.breadcrumb.meta?.clickable ?? false
+})
 </script>
 
 <template>
   <li class="breadcrumb-item">
-    <FieldText element="h5" class="title" :text="props.breadcrumb.meta.title" />
+    <NuxtLink v-if="isClickable" :to="props.breadcrumb.path" class="link">
+      <FieldText
+        element="h5"
+        class="title"
+        :text="props.breadcrumb.meta.title"
+      />
+    </NuxtLink>
+    <FieldText
+      v-else
+      element="h5"
+      class="title"
+      :text="props.breadcrumb.meta.title"
+    />
   </li>
 </template>
 
