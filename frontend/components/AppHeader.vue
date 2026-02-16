8c4778b0ab4689601ae7583a3edd598baa79400c
npnpnpnpnpnp <19845311+npnpnpnpnpnp@users.noreply.github.com>
2026-02-16 10:49:26 +0100

    Init navigation overlay

--- frontend/components/AppHeader.vue
+++ frontend/components/AppHeader.vue
@@ -1,18 +1,78 @@
 <script lang="ts" setup>
-const defaultsStore = useDefaultsStore()
-const { defaults } = storeToRefs(defaultsStore)
+// const defaultsStore = useDefaultsStore()
+// const { defaults } = storeToRefs(defaultsStore)
+
+const navVisible = ref(true)
+
+const toggleNavigation = () => {
+  navVisible.value = !navVisible.value
+}
 </script>
 
 <template>
   <header class="app-header">
-    <h1>{{ defaults.appTitle }}</h1>
-    <NavList />
+    <!-- <h1>{{ defaults.appTitle }}</h1> -->
+    <SiteLogo />
+    <button class="button" @click="toggleNavigation">
+      <span v-if="navVisible" class="close" />
+      <span v-else class="open" />
+    </button>
+    <NavList :is-visible="navVisible" />
     <!-- <LanguageSwitch /> -->
   </header>
 </template>
 
 <style lang="scss" scoped>
-// .app-header {
-//   h1 {}
-// }
+.app-header {
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 1;
+  display: flex;
+  justify-content: center;
+  width: 100%;
+}
+
+.button {
+  @include button-reset;
+
+  position: absolute;
+  top: 0;
+  right: 0;
+  z-index: 3;
+
+  // padding: var(--gutter);
+
+  // @media (min-width: $medium) {
+  //   padding: var(--gutter) calc(var(--gutter) * 2) 0 var(--gutter);
+  // }
+}
+
+.open,
+.close {
+  // @extend %ff-sans;
+  // @extend %fs-nav;
+
+  display: inline-block;
+
+  &::before {
+    display: block;
+
+    // width: var(--gutter);
+    // height: var(--gutter);
+    content: '';
+  }
+}
+
+.open {
+  &::before {
+    content: '\2630';
+  }
+}
+
+.close {
+  &::before {
+    content: '\00D7';
+  }
+}
 </style>
--- frontend/components/NavList.vue
+++ frontend/components/NavList.vue
@@ -1,16 +1,22 @@
 <script lang="ts" setup>
 const defaultsStore = useDefaultsStore()
 const { defaults } = storeToRefs(defaultsStore)
+
+const props = defineProps<{
+  isVisible: boolean
+}>()
+
+const visibleRoutes = computed(() => {
+  return defaults.value.navigation.filter(
+    (item) => item.meta.template !== 'home',
+  )
+})
 </script>
 
 <template>
-  <nav class="nav">
+  <nav v-if="props.isVisible" class="nav" aria-label="Navigation">
     <ul class="nav-list">
-      <NavItem
-        v-for="item in defaults.navigation"
-        :key="item.name"
-        :item="item"
-      />
+      <NavItem v-for="item in visibleRoutes" :key="item.name" :item="item" />
     </ul>
   </nav>
 </template>
@@ -18,5 +24,13 @@ const { defaults } = storeToRefs(defaultsStore)
 <style lang="scss" scoped>
 .nav-list {
   @include list-reset;
+
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 2;
+  width: 100%;
+  height: 100%;
+  background-color: var(--white);
 }
 </style>
