eb73b8e3bb40ba53841ee3cedebb3a6f2aa7f08d
npnpnpnpnpnp <19845311+npnpnpnpnpnp@users.noreply.github.com>
2026-02-18 13:15:32 +0100

    Set layout for matrix table

--- frontend/components/TextRowItem.vue
+++ frontend/components/TextRowItem.vue
@@ -3,19 +3,22 @@ const props = defineProps<{
   row: RowItem
 }>()
 
-// makes sure row is only shown when all fields are popluated
+// makes sure row is only shown when all fields are populated
 const showRow = computed(() => {
   return props.row?.key.length > 0 && props.row?.value.length > 0
 })
 </script>
 
 <template>
-  <li v-if="showRow" class="textrow-item">
+  <li v-if="showRow" class="text-row-item">
     <FieldText :text="props.row.key" class="row-key" />
     <FieldText :text="props.row.value" class="row-value" />
   </li>
 </template>
 
 <style lang="scss" scoped>
-// .text-row-item {}
+.text-row-item {
+  display: grid;
+  grid-template-columns: 30% 1fr;
+}
 </style>
