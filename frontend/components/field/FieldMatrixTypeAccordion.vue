23d8553962296d02bdcf98397783a4bb90d0304c
npnpnpnpnpnp <19845311+npnpnpnpnpnp@users.noreply.github.com>
2026-02-23 08:40:57 +0100

    Init content accordion toggling

--- frontend/components/field/FieldMatrixTypeAccordion.vue
+++ frontend/components/field/FieldMatrixTypeAccordion.vue
@@ -3,20 +3,36 @@ const props = defineProps<{
   item: MatrixTypeAccordion
 }>()
 
+const accordionVisible = ref(false)
+
 const showAccordion = computed(() => {
   return props.item.accordion != null
 })
+
+const toggleAccordion = () => {
+  accordionVisible.value = !accordionVisible.value
+}
 </script>
 
 <template>
   <div v-if="showAccordion" class="field-matrix-type-accordion">
-    <FieldText element="h4" :text="props.item.accordion.title" />
+    <FieldText
+      element="h4"
+      :text="props.item.accordion.title"
+      class="title"
+      @click="toggleAccordion"
+    />
     <FieldText
       v-if="props.item.accordion.subtitle"
       element="h5"
       :text="props.item.accordion.subtitle"
+      class="subtitle"
     />
-    <FieldMatrix :items="props.item.accordion.content" />
+    <FieldMatrix
+      v-if="accordionVisible"
+      :items="props.item.accordion.content"
+      class="content"
+    />
   </div>
 </template>
 
