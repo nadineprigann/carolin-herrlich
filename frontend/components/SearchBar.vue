facf552b4d6e74b6acea5401ade75faf210d4317
npnpnpnpnpnp <19845311+npnpnpnpnpnp@users.noreply.github.com>
2026-02-23 09:37:05 +0100

    Init filter bar

---
+++ frontend/components/SearchBar.vue
@@ -0,0 +1,49 @@
+<script lang="ts" setup>
+const props = defineProps<{
+  // note: Note
+}>()
+
+const labels = reactive({
+  placeholder: 'Suchen...',
+})
+</script>
+
+<template>
+  <!-- TODO: see insert -> search.vue -->
+  <!-- <section class="search-bar"> -->
+  <form class="search-bar" @submit.prevent>
+    <input
+      id="search"
+      :placeholder="labels.placeholder"
+      autocomplete="off"
+      class="input"
+      @input="debounceInput"
+    />
+    <button type="reset" class="reset" @click="clearResults" />
+  </form>
+  <!-- </section> -->
+</template>
+
+<style lang="scss" scoped>
+.search-bar {
+  display: grid;
+  grid-template-columns: minmax(0, 1fr) auto;
+}
+
+.input {
+  @include input-default;
+
+  // width: 100%;
+}
+
+.reset {
+  &::before {
+    display: block;
+
+    // width: var(--gutter);
+    // height: var(--gutter);
+    // content: '\2715'; // larger multiplication sign
+    content: '\00d7';
+  }
+}
+</style>
