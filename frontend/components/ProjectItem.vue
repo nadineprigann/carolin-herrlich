0649747a206d0bda285988928daa0e4a886490d2
npnpnpnpnpnp <19845311+npnpnpnpnpnp@users.noreply.github.com>
2026-02-24 08:39:41 +0100

    Init projects layout

--- frontend/components/ProjectItem.vue
+++ frontend/components/ProjectItem.vue
@@ -12,12 +12,19 @@ const showChild = computed(() => {
 <template>
   <li v-if="showChild" class="project-item">
     <NuxtLink :to="props.child.meta.url" class="link">
-      <FieldImage
-        v-if="props.child?.fields.image"
-        :image="props.child.fields.image"
-        class="image"
-      />
-      <FieldText class="title" element="h4" :text="props.child.fields.title" />
+      <div class="content">
+        <FieldImage
+          v-if="props.child?.fields.image"
+          :image="props.child.fields.image"
+          class="image"
+          :show-caption="false"
+        />
+        <FieldText
+          class="title"
+          element="h4"
+          :text="props.child.fields.title"
+        />
+      </div>
     </NuxtLink>
   </li>
 </template>
@@ -25,5 +32,49 @@ const showChild = computed(() => {
 <style lang="scss" scoped>
 .project-item {
   // @include link-default;
+  display: flex;
+  height: 30vh;
+
+  // TODO: how to set this within field img OR how to control overrides better? on screens above mobile, all images should be cover, is-portrait as well
+  :deep(img) {
+    max-width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  :deep(.is-portrait img) {
+    object-fit: contain;
+  }
+
+  @media (min-width: $mobile) {
+    margin: 0 auto; // centers the item if it has less width than the container, e.g. on mobile
+
+    /* portrait class is on the FIGURE, not on the IMG */
+    :deep(.is-portrait img) {
+      object-fit: cover;
+    }
+  }
+}
+
+.link {
+  @include link-default;
+
+  display: block;
+  width: 100%;
+  height: 100%;
+}
+
+.content {
+  display: grid;
+  grid-template-rows: 1fr auto;
+  height: 100%;
+}
+
+.title {
+  justify-self: center;
+
+  @media (min-width: $mobile) {
+    justify-self: normal;
+  }
 }
 </style>
