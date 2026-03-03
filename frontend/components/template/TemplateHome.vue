658e24c99cd6068d766493a52838d21d5d050326
npnpnpnpnpnp <19845311+npnpnpnpnpnp@users.noreply.github.com>
2026-03-03 16:10:41 +0100

    Define layout for home subtitle

--- frontend/components/template/TemplateHome.vue
+++ frontend/components/template/TemplateHome.vue
@@ -46,6 +46,19 @@ const { fields } = toRefs(props.data)
   @include fs-large;
 
   text-align: center;
+
+  @media (min-width: $xsmall) {
+    max-width: 70vw;
+    margin: 0 auto;
+  }
+
+  @media (min-width: $tablet) {
+    max-width: 55vw;
+  }
+
+  @media (min-width: $desktop) {
+    max-width: 40vw;
+  }
 }
 
 .links {
