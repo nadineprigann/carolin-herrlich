53d2eeac6ba7fb6388378d68b3ce35f30691cfad
npnpnpnpnpnp <19845311+npnpnpnpnpnp@users.noreply.github.com>
2026-03-04 11:33:32 +0100

    Do not show whole item when content does not exist

--- frontend/components/InDepthItem.vue
+++ frontend/components/InDepthItem.vue
@@ -9,8 +9,8 @@ const showItem = computed(() => {
 </script>
 
 <template>
-  <li class="in-depth-item">
-    <NuxtLink v-if="showItem" :to="props.item.meta.url" class="link">
+  <li v-if="showItem" class="in-depth-item">
+    <NuxtLink :to="props.item.meta.url" class="link">
       <FieldText element="h5" class="title" :text="props.item.fields.title" />
     </NuxtLink>
   </li>
