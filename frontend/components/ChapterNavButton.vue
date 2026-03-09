e15a1ba13a5053e2ec53e8f4b35dcb725fd13372
npnpnpnpnpnp <19845311+npnpnpnpnpnp@users.noreply.github.com>
2026-03-09 16:52:54 +0100

    Make sure chapter nav buttons wrap when they have long labels

--- frontend/components/ChapterNavButton.vue
+++ frontend/components/ChapterNavButton.vue
@@ -45,13 +45,15 @@ const navigate = () => {
 <style lang="scss" scoped>
 .chapter-nav-button {
   @include button-reset;
-  @include highlight-element($radius: 1.5em);
+  @include highlight-element($radius: 3rem);
   @include button-padding(
     $top: 0.4em,
     $bottom: var(--spacing-xs),
     $left: var(--spacing-l),
     $right: var(--spacing-l)
   );
+
+  max-width: 100%;
 
   &.is-prev {
     grid-column: 1;
@@ -67,6 +69,10 @@ const navigate = () => {
 .title {
   display: flex;
   align-items: center;
+  min-width: 0; // to allow the title to shrink and prevent overflow of the button, especially for long titles, in combination with overflow-wrap
+  hyphens: auto; // to allow hyphenation of long words for better breaking and wrapping of text, especially for long titles
+  word-break: break-word; // better than overflow-wrap, to prevent overflow of long titles, allow breaking and wrapping of text
+  white-space: normal;
 
   &::before,
   &::after {
