06b3503206cec76eaefd711a8e0f258212a7dff5
npnpnpnpnpnp <19845311+npnpnpnpnpnp@users.noreply.github.com>
2026-03-09 13:30:57 +0100

    Move globals to more specific definition

--- frontend/components/field/FieldTextarea.vue
+++ frontend/components/field/FieldTextarea.vue
@@ -18,8 +18,16 @@ const props = defineProps<{
 .field-textarea {
   // :deep() is needed to expose DOM elements inside a v-html
   :deep(h3),
-  :deep(h4) {
+  :deep(h4),
+  :deep(h6) {
     margin-bottom: var(--blank-line);
+  }
+
+  :deep(h6) {
+    @include ff-sans;
+    @include fw-bold;
+
+    margin-top: calc(var(--blank-line) * 2);
   }
 
   :deep(p) {
