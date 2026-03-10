aa0405fd926ac8e30b38edafc2988178ea35f25f
npnpnpnpnpnp <19845311+npnpnpnpnpnp@users.noreply.github.com>
2026-03-10 14:44:16 +0100

    Adjust basic page spacing

--- frontend/components/template/TemplateBasicPage.vue
+++ frontend/components/template/TemplateBasicPage.vue
@@ -17,20 +17,33 @@ const { fields, breadcrumbs } = toRefs(props.data)
 <template>
   <main class="template-basic-page">
     <BreadcrumbList :breadcrumbs="breadcrumbs" />
-    <FieldText element="h2" :text="fields.title" class="title" />
-    <FieldText
-      v-if="fields.subtitle"
-      element="h3"
-      :text="fields.subtitle"
-      class="subtitle"
-    />
+    <div class="header">
+      <FieldText element="h2" :text="fields.title" class="title" />
+      <FieldText
+        v-if="fields.subtitle"
+        element="h3"
+        :text="fields.subtitle"
+        class="subtitle"
+      />
+    </div>
     <FieldMatrix :items="fields.content" />
     <RelatedContent :related="fields.related_content" />
   </main>
 </template>
 
 <style lang="scss" scoped>
-// .template-basic-page {}
+.template-basic-page {
+  padding: var(--page-spacing);
+}
+
+.header {
+  margin-bottom: var(--gutter-xl);
+
+  @media (min-width: $medium) {
+    margin-bottom: calc(var(--gutter-base) * 5);
+  }
+}
+
 .title,
 .subtitle {
   @include center-content;
