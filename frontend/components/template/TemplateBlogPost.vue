107762b94a9d132b0a5fd8cbeb5fe574af2f5531
npnpnpnpnpnp <19845311+npnpnpnpnpnp@users.noreply.github.com>
2026-03-10 13:27:24 +0100

    Set spacing on blog templates

--- frontend/components/template/TemplateBlogPost.vue
+++ frontend/components/template/TemplateBlogPost.vue
@@ -13,20 +13,32 @@ const { fields, breadcrumbs } = toRefs(props.data)
 <template>
   <main class="template-blog-post">
     <BreadcrumbList :breadcrumbs="breadcrumbs" />
-    <FieldText element="h2" :text="fields.title" class="title" />
-    <FieldText
-      v-if="fields.subtitle"
-      element="h3"
-      :text="fields.subtitle"
-      class="subtitle"
-    />
+    <div class="header">
+      <FieldText element="h2" :text="fields.title" class="title" />
+      <FieldText
+        v-if="fields.subtitle"
+        element="h3"
+        :text="fields.subtitle"
+        class="subtitle"
+      />
+    </div>
     <FieldMatrix :items="fields.content" />
     <RelatedContent :related="fields.related_content" />
   </main>
 </template>
 
 <style lang="scss" scoped>
-// .template-blog-post {}
+.template-blog-post {
+  padding: var(--page-spacing);
+}
+
+.header {
+  margin-bottom: var(--gutter-xl);
+
+  @media (min-width: $medium) {
+    margin-bottom: calc(var(--gutter-base) * 5);
+  }
+}
 
 .title,
 .subtitle {
