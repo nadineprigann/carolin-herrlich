51f93c6e62003c8a42ae3f529da72a4c1abe5312
npnpnpnpnpnp <19845311+npnpnpnpnpnp@users.noreply.github.com>
2026-04-14 11:59:00 +0200

    Create UX feedback for filter button when filters are applied

--- frontend/components/FilterBar.vue
+++ frontend/components/FilterBar.vue
@@ -1,13 +1,26 @@
 <script lang="ts" setup>
+const formStore = useFormStore()
+const { selected } = storeToRefs(formStore)
+
 const props = defineProps<{
   // item:
   overlay: 'checkout' | 'filter'
 }>()
+
+const hasFilters = computed(() => {
+  return selected.value.categories.length > 0
+})
+
+const classes = computed(() => {
+  return {
+    button: ['button', hasFilters.value ? 'is-active' : ''],
+  }
+})
 </script>
 
 <template>
   <section class="filter-bar">
-    <OverlayButton :overlay="props.overlay" />
+    <OverlayButton :overlay="props.overlay" :class="classes.button" />
     <!-- <SearchBar /> -->
   </section>
 </template>
@@ -21,6 +34,22 @@ const props = defineProps<{
 
   @media (min-width: $medium) {
     margin-bottom: calc(var(--gutter-base) * 5);
+  }
+}
+
+.button {
+  &.is-active {
+    position: relative;
+
+    &::after {
+      position: absolute;
+      top: 0;
+      right: 0;
+      padding: var(--spacing-xxs);
+      content: '';
+      background-color: var(--highlight-color);
+      border-radius: 100%;
+    }
   }
 }
 </style>
